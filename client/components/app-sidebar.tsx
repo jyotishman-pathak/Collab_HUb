545b711d73aa38e3abb2e5e770c4ccc622916b66
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2024-10-19 21:11:42 +0530

    added cool dash

---
+++ client/components/app-sidebar.tsx
@@ -0,0 +1,219 @@
+import * as React from "react"
+import { GalleryVerticalEnd } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Dashboard Overview",
+      url: "/dashboard",
+      items: [
+        {
+          title: "Profile Overview",
+          url: "/dashboard/profile",
+        },
+        {
+          title: "Recent Activity",
+          url: "/dashboard/activity",
+        },
+      ],
+    },
+    {
+      title: "My Courses",
+      url: "/courses",
+      items: [
+        {
+          title: "Enrolled Courses",
+          url: "/courses/enrolled",
+        },
+        {
+          title: "Course Progress",
+          url: "/courses/progress",
+        },
+      ],
+    },
+    {
+      title: "Assignments",
+      url: "/assignments",
+      items: [
+        {
+          title: "Upcoming Assignments",
+          url: "/assignments/upcoming",
+        },
+        {
+          title: "Submitted Assignments",
+          url: "/assignments/submitted",
+        },
+      ],
+    },
+    {
+      title: "Grades",
+      url: "/grades",
+      items: [
+        {
+          title: "View Grades",
+          url: "/grades/view",
+        },
+        {
+          isActive:true,
+          title: "Grade History",
+          url: "/grades/history",
+        },
+      ],
+    },
+    {
+  
+      title: "Class Schedule",
+      url: "/schedule",
+      items: [
+        {
+          title: "Weekly Schedule",
+          url: "/schedule/weekly",
+        },
+        {
+          title: "Exams Schedule",
+          url: "/schedule/exams",
+        },
+      ],
+    },
+    {
+      title: "Messages",
+      url: "/messages",
+      items: [
+        {
+          title: "Inbox",
+          url: "/messages/inbox",
+        },
+        {
+          title: "Sent",
+          url: "/messages/sent",
+        },
+      ],
+    },
+    {
+      title: "Study Resources",
+      url: "/resources",
+      items: [
+        {
+          title: "Lecture Notes",
+          url: "/resources/notes",
+        },
+        {
+         
+          title: "Tutorials",
+          url: "/resources/tutorials",
+        },
+      ],
+    },
+    {
+      title: "Events",
+      url: "/events",
+      items: [
+        {
+          title: "Workshops",
+          url: "/events/workshops",
+        },
+        {
+          title: "Seminars",
+          url: "/events/seminars",
+        },
+      ],
+    },
+    {
+      title: "Profile Settings",
+      url: "/settings",
+      items: [
+        {
+          title: "Account Information",
+          url: "/settings/account",
+        },
+        {
+          title: "Change Password",
+          url: "/settings/password",
+        },
+      ],
+    },
+    {
+      title: "Support",
+      url: "/support",
+      items: [
+        {
+          title: "Contact Support",
+          url: "/support/contact",
+        },
+        {
+          title: "FAQs",
+          url: "/support/faqs",
+        },
+      ],
+    },
+    {
+      title: "Logout",
+      url: "/logout",
+    },
+  ],
+}
+
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="floating" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Documentation</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-2">
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub className="ml-0 border-l-0 px-1.5">
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
---
+++ client/components/dashboard/student/Stats.tsx
@@ -0,0 +1,122 @@
+"use client"
+
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A donut chart with text"
+
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 190, fill: "var(--color-other)" },
+]
+
+const chartConfig = {
+  visitors: {
+    label: "attendence",
+  },
+  chrome: {
+    label: "BST",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Digita Circuits",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Physics",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Chemistry",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Networks",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig
+
+export function Stats() {
+  const totalVisitors = React.useMemo(() => {
+    return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+  }, [])
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Attendece- Raj Vimani</CardTitle>
+        <CardDescription>Jan 2024 - Present</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[230px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {totalVisitors/100 }
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Attendence
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    
+    </Card>
+  )
+}
